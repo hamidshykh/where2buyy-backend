e1a2ae796e37a257b6647f032914b4c8fb2d886d
Hamid Shaikh <128905142+hamidshykh@users.noreply.github.com>
2026-05-17 16:41:25 +0500

    Modify startup.sh to use PORT environment variable
    Update startup script to use dynamic PORT variable.

--- startup.sh
+++ startup.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 set -e
 
-# Activate the virtual environment
-source /antenv/bin/activate
-
-# Start the app with uvicorn
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000
+exec uvicorn app.main:app --host 0.0.0.0 --port "${PORT:-8000}"
